0b17d2c2f31c0521da081c9770dee56ff8bf5669
Areeba Naeem <areebasaifi6844@gmail.com>
2025-12-20 21:28:03 +0000

    1 change

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,7 +1,7 @@
 def twosums(n,t):
     for i in range(len(n)):
         for j in range( i+1, len(n)):
-            if n[i] + n[j] == t
+            if n[i] + n[j] == t:
                 return [i,j]
 
 print(twosums([2,8,9,10,11,10,27,19], 9))
